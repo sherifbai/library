bae5862ce43fd7ed995c65f988f0c65882dae414
Sherif <godness980@gmail.com>
2021-10-01 17:19:58 +0500

    logic for genres

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\Book
@@ -12,24 +16,30 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $author
  * @property string $genre
  * @property string $publisher
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|Book newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Book newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Book query()
- * @method static \Illuminate\Database\Eloquent\Builder|Book whereAuthor($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Book whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Book whereGenre($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Book whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Book wherePublisher($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Book whereUpdatedAt($value)
- * @mixin \Eloquent
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Book newModelQuery()
+ * @method static Builder|Book newQuery()
+ * @method static Builder|Book query()
+ * @method static Builder|Book whereAuthor($value)
+ * @method static Builder|Book whereCreatedAt($value)
+ * @method static Builder|Book whereGenre($value)
+ * @method static Builder|Book whereId($value)
+ * @method static Builder|Book wherePublisher($value)
+ * @method static Builder|Book whereUpdatedAt($value)
+ * @mixin Eloquent
  * @property string $name
- * @method static \Illuminate\Database\Eloquent\Builder|Book whereName($value)
+ * @method static Builder|Book whereName($value)
+ * @property-read int|null $genre_count
  */
 class Book extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'author', 'genre', 'publisher'];
+    protected $fillable = ['name', 'author', 'publisher'];
+
+    public function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class,'book_has_genres');
+    }
 }
